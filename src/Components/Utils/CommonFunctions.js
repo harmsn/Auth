4f164f61fdc8215652452c8857a75efc5009d948
harmsn <harmangill755@gmail.com>
2021-12-26 20:44:08 +0530

    Removed comented code

--- src/Components/Utils/CommonFunctions.js
+++ src/Components/Utils/CommonFunctions.js
@@ -2,17 +2,14 @@ import { message } from 'antd';
 
 const success = (msg) => {
     message.success(msg);
-    //alert(msg);
 };
   
 const error = (msg) => {
     message.error(msg);
-    //alert(msg);
 };
   
 const warning = (msg) => {
     message.warning(msg);
-    //alert(msg)
 };
 
 const debounce = (func, delay) => {
